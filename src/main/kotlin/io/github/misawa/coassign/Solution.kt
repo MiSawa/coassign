3f985b3217bcc50af3e1d016ac116e1ccc45877f
mi_sawa <mi.sawa.1216+git@gmail.com>
2019-12-09 23:12:28 +0900

    Add a method to get the solution

--- src/main/kotlin/io/github/misawa/coassign/Solution.kt
+++ src/main/kotlin/io/github/misawa/coassign/Solution.kt
@@ -1,6 +1,5 @@
 package io.github.misawa.coassign
 
-import io.github.misawa.coassign.utils.StatsTracker
 import kotlin.math.max
 
 class Solution(
@@ -16,14 +15,24 @@ class Solution(
         value = res
     }
 
-    fun check() {
+    fun getMatches(): List<Pair<Int, Int>> = graph.forwardEdges.filter { used[it] }.map {
+        if (graph.flipped) {
+            ((graph.targets[it] - graph.lSize) to graph.sources[it])
+        } else {
+            (graph.sources[it] to (graph.targets[it] - graph.lSize))
+        }
+    }
+
+    fun checkConstraints() {
         val matchCount = IntArray(graph.numV)
         for (e in graph.forwardEdges) if (used[e]) {
             ++matchCount[graph.sources[e]]
             ++matchCount[graph.targets[e]]
         }
         for (u in graph.nodes) check(matchCount[u] <= graph.multiplicities[u]) { "The matching includes too many edges adjacent to $u" }
+    }
 
+    fun checkOptimality() {
         for (e in graph.edges) {
             val rWeight = graph.weights[e] - potential[graph.sources[e]] + potential[graph.targets[e]]
             if (rWeight > 0) check(used[e]) { "There's an edge that has positive residual weight but not used" }
@@ -32,7 +41,15 @@ class Solution(
         fun sgn(u: Node): Int = if (u < graph.lSize) 1 else -1
         var dual: LargeWeight = 0
         for (u in graph.nodes) dual += graph.multiplicities[u] * max(0, sgn(u) * potential[u]).toLargeWeight()
-        for (e in graph.forwardEdges) dual += max(0, graph.weights[e] - potential[graph.sources[e]] + potential[graph.targets[e]])
+        for (e in graph.forwardEdges) dual += max(
+            0,
+            graph.weights[e] - potential[graph.sources[e]] + potential[graph.targets[e]]
+        )
         check(value == dual) { "Primal and dual value should be the same. Primal: $value, dual: $dual" }
+    }
+
+    fun check() {
+        checkConstraints()
+        checkOptimality()
     }
 }
